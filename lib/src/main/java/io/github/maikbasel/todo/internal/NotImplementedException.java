c7ada4c8e281a2685b564849334442da81db74ff
Maik <maik.basel@gmx.de>
2024-01-23 21:44:31 +0100

    feat: Add todo method with corresponding test
    Added `NotImplementedException` class that is thrown by `Dev.todo()`
    methods to indicate unimplemented areas in the project. The exception
     either uses a default message or accepts a custom message.
     Corresponding test methods have been added to 'DevTest' to
     demonstrate its usage.

--- lib/src/main/java/io/github/maikbasel/todo/internal/NotImplementedException.java
+++ lib/src/main/java/io/github/maikbasel/todo/internal/NotImplementedException.java
@@ -3,6 +3,7 @@ package io.github.maikbasel.todo.internal;
 public class NotImplementedException extends RuntimeException {
 
     public NotImplementedException() {
+        super("Not implemented yet.");
     }
 
     public NotImplementedException(String message) {
