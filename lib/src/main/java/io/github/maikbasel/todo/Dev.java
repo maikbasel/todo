9751a4ded72da1be7cedfa15940ecd644b268b36
Maik <maik.basel@gmx.de>
2024-01-23 22:10:38 +0100

    docs: Enhance documentation for 'io.github.maikbasel.todo' module and 'Dev' class
    Improved the clarity of the 'io.github.maikbasel.todo' module's
    definition and its use cases. Added detailed comments to 'Dev' class,
     specifically for methods that throw 'NotImplementedException'.
     Methods that have not been implemented now include descriptions for
     better understanding and use.

--- lib/src/main/java/io/github/maikbasel/todo/Dev.java
+++ lib/src/main/java/io/github/maikbasel/todo/Dev.java
@@ -2,15 +2,34 @@ package io.github.maikbasel.todo;
 
 import io.github.maikbasel.todo.internal.NotImplementedException;
 
+/**
+ * The Dev class provides methods for marking a method as a placeholder for future implementation.
+ * When called, the marked method will throw a {@link NotImplementedException}.
+ * Use the methods in this class to indicate that a certain functionality is not yet implemented.
+ */
 public final class Dev {
 
     private Dev() {
     }
 
+    /**
+     * Marks a method as a placeholder for future implementation. When called, it will throw a {@link NotImplementedException}.
+     * Use this method to indicate that a certain functionality is not yet implemented.
+     *
+     * @throws NotImplementedException when the method is called
+     */
     public static void todo() {
         throw new NotImplementedException();
     }
 
+    /**
+     * Marks a method as a placeholder for future implementation.
+     * When called, it will throw a {@link NotImplementedException}.
+     * Use this method to indicate that a certain functionality is not yet implemented.
+     *
+     * @param message to be included in the {@link NotImplementedException} when the method is called.
+     * @throws NotImplementedException when the method is called
+     */
     public static void todo(String message) {
         throw new NotImplementedException(message);
     }
--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,3 +1,10 @@
-module io.github.maikbasel.todo {
+/**
+ * This module defines the package "io.github.maikbasel.todo" and exports it for other modules to use.
+ * The "io.github.maikbasel.todo" package may contain classes and interfaces related to managing and organizing todo items.
+ * <p>
+ * It does not define any specific classes or interfaces within the module.
+ */ /**
+ *
+ */module io.github.maikbasel.todo {
     exports io.github.maikbasel.todo;
 }
