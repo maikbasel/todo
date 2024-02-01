8f03b8419b5cb47b369513bd4a6cfa0edf1b4c00
Maik <maik.basel@gmx.de>
2024-02-01 23:11:05 +0100

    ci(release): Set up Maven publishing and signing for project
    Configured Maven publish and signing plugins in the build.gradle.kts
    file for secure publication of project artifacts to Maven
    repositories. Additionally, a new Release GitHub Actions workflow has
     been added to automate these publishing and releasing processes.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     `java-library`
-    id("io.freefair.aspectj") version("6.3.0")
+    `maven-publish`
+    `signing`
+    id("io.freefair.aspectj") version ("6.3.0")
 }
+
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -18,8 +22,89 @@ dependencies {
 }
 
 java {
+    withJavadocJar()
+    withSourcesJar()
+
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    val extension = extensions
+        .getByName("publishing") as PublishingExtension
+    sign(extension.publications)
+}
+
+val isReleaseVersion by extra { version.toString().endsWith("SNAPSHOT").not() }
+
+object Meta {
+    const val DESC = "A Java adaptation of Rust's std::todo macro features using static factory methods, AspectJ's AOP, and annotations to mark unimplemented code sections."
+    const val LICENSE = "MIT"
+    const val GITHUB_REPO = "maikbasel/todo"
+    const val RELEASE_REPO = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+    const val SNAPSHOT_REPO = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            pom {
+                name.set(project.name)
+                description.set(Meta.DESC)
+                url.set("https://github.com/${Meta.GITHUB_REPO}")
+                licenses {
+                    license {
+                        name.set(Meta.LICENSE)
+                        url.set("https://opensource.org/license/mit")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("maikbasel")
+                        name.set("Maik Basel")
+                    }
+                }
+                scm {
+                    url.set(
+                        "https://github.com/${Meta.GITHUB_REPO}.git"
+                    )
+                    connection.set(
+                        "scm:git:git://github.com/${Meta.GITHUB_REPO}.git"
+                    )
+                    developerConnection.set(
+                        "scm:git:git://github.com/${Meta.GITHUB_REPO}.git"
+                    )
+                }
+                issueManagement {
+                    url.set("https://github.com/${Meta.GITHUB_REPO}/issues")
+                }
+            }
+
+            repositories {
+                maven {
+                    url = if (isReleaseVersion) uri(Meta.RELEASE_REPO) else uri(Meta.SNAPSHOT_REPO)
+
+                    credentials {
+                        val ossrhUsername: String? by project
+                        val ossrhPassword: String? by project
+
+                        username = ossrhUsername
+                        password = ossrhPassword
+                    }
+                }
+            }
+        }
     }
 }
 
